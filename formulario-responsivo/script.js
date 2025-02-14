7778731cbc34ef64d13e49ba796d653a1ea9ccde
Pssiqueira <augustosiqueira7@gmail.com>
2025-02-14 16:59:41 -0300

    att form

--- formulario-responsivo/script.js
+++
@@ -1,6 +0,0 @@
- var mail = document.getElementById('mail')
- var lmail = document.querySelector('label')
-var sp = document.querySelector('span')
- mail.addEventListener("click",() =>{
-    sp.style.top = '-10px'
-})
