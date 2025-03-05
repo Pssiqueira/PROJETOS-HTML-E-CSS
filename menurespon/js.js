a405b21eefeaab52a1ac5ebff334e304ed397405
Pssiqueira <augustosiqueira7@gmail.com>
2025-03-05 17:20:11 -0300

    att mq

--- menurespon/js.js
+++ menurespon/js.js
@@ -1,10 +1,12 @@
 var nav = document.getElementById('nav')
 var lista = document.getElementById('lista')
 nav.addEventListener("click", ()=>{
-    if(lista.style.left == '10px'){
-        lista.style.left = '-100px'
-    }else{
-        lista.style.left = '10px'
+    if(window.innerWidth < 768){
+        if(lista.style.left == '10px'){
+            lista.style.left = '-100px'
+        }else{
+            lista.style.left = '10px'
+        }
     }
-    
 })
+
