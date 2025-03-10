8e6d7a88212d7359c3453c02de36ad32b45b2c46
Pssiqueira <augustosiqueira7@gmail.com>
2025-03-09 23:26:36 -0300

    att console

--- siteconsole/js.js
+++ siteconsole/js.js
@@ -1,8 +1,15 @@
 var navmain = document.getElementById('nav-main')
+var down = document.getElementById('down')
+var sear = document.getElementById('sear')
 function menu(){
+    down.classList.add('slide')
+    sear.classList.add('slide2')
     if(navmain.style.left == '-200px'){
         navmain.style.left = '0'
     }else{
         navmain.style.left = '-200px'
+        down.classList.remove('slide')
+        sear.classList.remove('slide2')
     }
+    
 }
