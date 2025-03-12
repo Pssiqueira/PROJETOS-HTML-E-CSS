cd82854a0474e2089ac9420a1eb281fd9d4e5769
Pssiqueira <augustosiqueira7@gmail.com>
2025-03-12 17:21:02 -0300

    att sidebar

--- siteconsole/js.js
+++ siteconsole/js.js
@@ -1,15 +1,1 @@
-var navmain = document.getElementById('nav-mai')
-var down = document.getElementById('dow')
-var sear = document.getElementById('sea')
-function menu(){
-    down.classList.add('slide')
-    sear.classList.add('slide2')
-    if(navmain.style.left == '-200px'){
-        navmain.style.left = '0'
-    }else{
-        navmain.style.left = '-200px'
-        down.classList.remove('slide')
-        sear.classList.remove('slide2')
-    }
-    
-}
+
